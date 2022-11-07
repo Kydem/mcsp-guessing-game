efc16f4d0f94e0f9baa632e93de45aed6c1f1013
Kydem <kydemartino1999@gmail.com>
2022-11-07 15:56:35 -0700

    Feature 4 complete

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 let guess;
 let goal = Math.floor(Math.random() * 10) + 1
 let attemptCount = 0
-let prevGuess = []
-let listGuess = prevGuess.toString()
+const prevGuess = []
 
 function play(){
     while(true) {
         guess = prompt("Guess a number between 1 and 10")
         let numGuess = Number(guess)
+        prevGuess.push(guess)
         if (guess === null) {
             alert("Goodbye")
+            break;
         }
 
         if (numGuess === goal) {
@@ -18,21 +19,18 @@ function play(){
                 alert(`That's right! You got it on the first try!`)
                 break;
             } else {
-                alert(`That's right! It took you ${attemptCount} tries. \nYour previous answers were ${listGuess}`)
+                alert(`That's right! It took you ${attemptCount} tries. \nYour previous answers were ${prevGuess.join(", ")}`)
                 break;
             }
         } else if (numGuess > goal && numGuess <= 10) {
-            prevGuess.push(guess)
             attemptCount++
-            alert(`Attempt Number: ${attemptCount} \nThe number is lower than your guess. Try again`)
+            alert(`Attempt Number: ${attemptCount} \nThe number is lower than your guess. Try again`);
         } else if (numGuess < goal && numGuess >= 1) {
-            prevGuess.push(guess)
             attemptCount++
-            alert(`Attempt Number: ${attemptCount} \nThe number is higher than your guess. Try again`)
+            alert(`Attempt Number: ${attemptCount} \nThe number is higher than your guess. Try again`);
         } else {
-            prevGuess.push(guess)
             attemptCount++
-            alert(`Attempt Number: ${attemptCount} \nThis is not a number between 1 and 100. Try again`)
+            alert(`Attempt Number: ${attemptCount} \nThis is not a number between 1 and 100. Try again`);
         }
     }
 }
