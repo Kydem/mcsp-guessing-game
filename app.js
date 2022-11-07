9cfc24a48781bb324ef70e0ea020985d18227386
Kydem <kydemartino1999@gmail.com>
2022-11-07 14:45:59 -0700

    Functioning Game with attempt counter

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-play(); // This is your main function that runs when the page loads
+let guess;
+let goal = 47
+let attemptCount = 0
 
 function play(){
-   
+    guess = Number(prompt("Guess a number between 1 and 100"))
+    while(true) {
+        if (guess === goal) {
+            attemptCount++
+            if (attemptCount === 1) {
+                alert(`That's right! You got it on the first try!`)
+                break;
+            } else {
+                alert(`That's right! It took you ${attemptCount} tries.`)
+                break;
+            }
+        } else if (guess > goal && guess <= 100) {
+            attemptCount++
+            guess = Number(prompt("The number is lower than your guess. Try again"))
+        } else if (guess < goal && guess >= 1) {
+            attemptCount++
+            guess = Number(prompt("The number is higher than your guess. Try again"))
+        } else {
+            attemptCount++
+            guess = Number(prompt("That is not a number between 1 and 100. Try again"))
+        }
+    }
 }
 
-function playAgain(){
-    
-}
+play(); // This is your main function that runs when the page loads
