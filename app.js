fd2988e5cff9997b0f591653ed0e9607f47fab27
Kydem <kydemartino1999@gmail.com>
2022-11-07 15:25:41 -0700

    Functions, count++, edgeCases

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 let guess;
-let goal = 47
+let goal = Math.floor(Math.random() * 10) + 1
 let attemptCount = 0
+let prevGuess = []
+let listGuess = prevGuess.toString()
 
 function play(){
-    guess = Number(prompt("Guess a number between 1 and 100"))
     while(true) {
-        if (guess === goal) {
+        guess = prompt("Guess a number between 1 and 10")
+        let numGuess = Number(guess)
+        if (guess === null) {
+            alert("Goodbye")
+        }
+
+        if (numGuess === goal) {
             attemptCount++
             if (attemptCount === 1) {
                 alert(`That's right! You got it on the first try!`)
                 break;
             } else {
-                alert(`That's right! It took you ${attemptCount} tries.`)
+                alert(`That's right! It took you ${attemptCount} tries. \nYour previous answers were ${listGuess}`)
                 break;
             }
-        } else if (guess > goal && guess <= 100) {
+        } else if (numGuess > goal && numGuess <= 10) {
+            prevGuess.push(guess)
             attemptCount++
-            guess = Number(prompt("The number is lower than your guess. Try again"))
-        } else if (guess < goal && guess >= 1) {
+            alert(`Attempt Number: ${attemptCount} \nThe number is lower than your guess. Try again`)
+        } else if (numGuess < goal && numGuess >= 1) {
+            prevGuess.push(guess)
             attemptCount++
-            guess = Number(prompt("The number is higher than your guess. Try again"))
+            alert(`Attempt Number: ${attemptCount} \nThe number is higher than your guess. Try again`)
         } else {
+            prevGuess.push(guess)
             attemptCount++
-            guess = Number(prompt("That is not a number between 1 and 100. Try again"))
+            alert(`Attempt Number: ${attemptCount} \nThis is not a number between 1 and 100. Try again`)
         }
     }
 }
