6c7a7235152b967415493fdbf0b96d909d8d04be
Kydem <kydemartino1999@gmail.com>
2022-11-08 13:15:53 -0700

    Built comments to assist in readability

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
+// This is a number guessing game! //
+
+//These are the variables that should not be reset on a new game
 let userName = prompt("Please enter your name.")
 if (userName === null) {
     userName = "Stranger"
 }
 
+//highScores will store the user name as a key, and their lowest number of guesses as the value
 let highScores = {};
 highScores[userName] = 0
 
+//This is the functioning game with 2 types of "win" cases and 3 types of "lose" cases. The game is repeated until a win case is met.
 function play(){
     let guess;
     let attemptCount = 0
     let prevGuess = []
+    //The secret number is inside the game function so it is a new number every time you play it
     let goal = Math.floor(Math.random() * 10) + 1
     while(true) {
         guess = prompt(`Ok ${userName}, guess a number between 1 and 10`)
         let numGuess = Number(guess)
         prevGuess.push(guess)
+        //If the game is ended early, the player will receive a "goodbye" message.
         if (guess === null) {
             alert(`Goodbye ${userName}.`)
             break;
         }
-
+        //Win and lose cases
         if (numGuess === goal) {
             attemptCount++
             if (attemptCount === 1) {
@@ -52,6 +59,7 @@ function play(){
             alert(`Attempt Number: ${attemptCount} \nThis is not a number between 1 and 100. Try again`);
         }
     }
+    //This is a prompt that will allow the player to play again if they want to
     playAgain = prompt(`Would you like to play again, ${userName}? \nType "yes" or "no".`)
     // use .confirm instead of prompt if prompt doesnt work
     if (playAgain.toLowerCase() === "yes") {
